7ae8dab30ae3074475e0b1c8fa68ecc748563eeb
Anastasia Dolotova <ad4019809@gmail.com>
2024-04-23 22:36:45 +0300

    ML-CLEVER-A-6: add image processing in pdfs

--- MLCore/Services/TextService.py
+++ MLCore/Services/TextService.py
@@ -4,9 +4,12 @@ from minio import Minio
 import logging
 from Processors import IDataProcessor
 from Processors.TextProcessor import TextProcessor
+from Processors.ImageProcessor import ImageProcessor
 from pymongo.collection import Collection
 from service_interfaces import IDataService
+from PIL import Image
 import sys
+import fitz
 sys.path.insert(0, './MLCore/utils')
 from utils.get_console_logger import get_console_logger
 
@@ -50,6 +53,23 @@ class TextService(IDataService):
 
         proc_list, pages = self.worker.process(local_file_path)
 
+        img_filenames = self.__process_images_in_file(local_file_path)
+        img_embedddings = []
+
+        logger.info(f'images in pdf: {len(img_filenames)}')
+
+        if len(img_filenames) > 0:
+            img_proc = ImageProcessor()
+            for filename in img_filenames:
+                embeddings = img_proc.process(
+                    Image.open(
+                        filename
+                    )
+                )
+                if embeddings:
+                    img_embedddings.append(embeddings)
+                os.remove(filename)
+
         os.remove(local_file_path)
 
         upd_query = {
@@ -57,7 +77,8 @@ class TextService(IDataService):
             {
                 'ml_data': {
                     'text_repr': proc_list,
-                    'page_number': pages
+                    'page_number': pages,
+                    'img_repr': img_embedddings
                 }
             }
         }
@@ -65,3 +86,25 @@ class TextService(IDataService):
         self.mongo_collection.update_one(
             {'_id': document['_id']}, upd_query
         )
+
+    def __process_images_in_file(self, local_file_path):
+        pdf_document = fitz.open(local_file_path)
+        img_filenames = []
+
+        for page_number in range(pdf_document.page_count):
+            page = pdf_document.load_page(page_number)
+            image_list = page.get_images(full=True)
+            
+            for image_index, image_info in enumerate(image_list):
+                xref = image_info[0]
+                base_image = pdf_document.extract_image(xref)
+                image_bytes = base_image["image"]
+                
+                image_filename = f'image_{page_number}_{image_index}.png'
+                image_file = open(image_filename, 'wb')
+                image_file.write(image_bytes)
+                image_file.close()
+                img_filenames.append(image_filename)
+
+        pdf_document.close()
+        return img_filenames
